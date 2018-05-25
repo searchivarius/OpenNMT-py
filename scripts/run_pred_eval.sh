8612284d117fb834fa5a4fe4091d138caa4f6533
Leonid Boytsov <leo@Leonids-MBP.fios-router.home>
2018-05-25 03:46:18 -0400

    finished eval script

--- scripts/run_pred_eval.sh
+++ scripts/run_pred_eval.sh
@@ -10,21 +10,30 @@ if [ ! -d "$DATA_DIR" ] ; then
 fi
 MODEL_DIR=$2
 if [ "$MODEL_DIR" = "" ] ; then
-    echo "Specify DATA model dir. (1d arg)"
+    echo "Specify DATA model dir. (2d arg)"
     exit 1
 fi
 if [ ! -d "$MODEL_DIR" ] ; then
     echo "Not a directory (2d arg)"
     exit 1
 fi
-COMP_OPT=$3
-if [ "$COMP_OPT" = "" ] ; then
-    echo "Specify a composition option (3rd arg)"
+PRED_DIR=$3
+if [ "PRED_DIR" = "" ] ; then
+    echo "Specify prediction model dir. (3rd arg)"
     exit 1
 fi
-EPOCH_QTY=$4
+if [ ! -d "PRED_DIR" ] ; then
+    echo "Not a directory (3rd arg)"
+    exit 1
+fi
+COMP_OPT=$4
+if [ "$COMP_OPT" = "" ] ; then
+    echo "Specify a composition option (4th arg)"
+    exit 1
+fi
+EPOCH_QTY=$5
 if [ "$EPOCH_QTY" = "" ] ; then
-    echo "Specify the number of epoch (4th arg)"
+    echo "Specify the number of epoch (5th arg)"
     exit 1
 fi
 
@@ -36,3 +45,7 @@ if [ "$?" != "0" ] ; then
     echo "Error finding the model or the model is not found!"
     exit 1
 fi
+
+PRED_FILE=$PRED_DIR/${COMP_OPT}.pred
+./translate.py -model "$MODEL_FULL_PATH" -src "$DATA_DIR/src-test.txt" -output "$PRED_FILE" -replace_unk -verbose
+tools/multi-bleu-detok.perl "$DATA_DIR/tgt-test.txt" < "$PRED_FILE"
